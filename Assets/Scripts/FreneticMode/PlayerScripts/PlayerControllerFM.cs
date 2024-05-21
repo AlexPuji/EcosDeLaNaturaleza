4b5b253567cd8f965c0c30eb231bf0832854661d
AlexPuji <apujolb@edu.tecnocampus.cat>
2024-05-21 19:13:42 +0200

    Retoques entrega 2

--- Assets/Scripts/FreneticMode/PlayerScripts/PlayerControllerFM.cs
+++ Assets/Scripts/FreneticMode/PlayerScripts/PlayerControllerFM.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerControllerFM : MonoBehaviour
 {
-    public float maxHealth = 100f; // Vida máxima del jugador
-    private float currentHealth; // Vida actual del jugador
+    public float maxHealth = 100f; 
+    private float currentHealth; 
 
-    public Slider healthSlider; // Referencia al Slider de vida en la interfaz de usuario
+    public Slider healthSlider; 
 
-    private static PlayerController instance;
+    private static PlayerControllerFM instance;
 
     
-
-    // Método llamado al inicio para inicializar la vida del jugador
     private void Start()
     {
         currentHealth = maxHealth;
 
-        // Actualiza el valor inicial del Slider de vida
+        
         if (healthSlider != null)
         {
             healthSlider.maxValue = maxHealth;
@@ -27,31 +26,33 @@ public class PlayerControllerFM : MonoBehaviour
         }
     }
 
-    // Método para que el jugador reciba daño
+    
     public void TakeDamage(float damageAmount)
     {
         currentHealth -= damageAmount;
         Debug.Log("Player takes damage. Current health: " + currentHealth);
 
-        // Actualiza el valor del Slider de vida
+        
         if (healthSlider != null)
         {
             healthSlider.value = currentHealth;
         }
 
-        // Verifica si el jugador ha perdido toda su vida y muere
+        
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
-    // Método llamado cuando el jugador muere
+    
     private void Die()
     {
         Debug.Log("Player has died.");
         gameObject.SetActive(false);
+
+        // Carga la escena "SelectMode"
+        SceneManager.LoadScene("SelectMode");
     }
 
-    
 }
